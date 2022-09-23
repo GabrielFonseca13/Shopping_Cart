86f2a9a99cd6de6940146528ebd2d067bef22a0c
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-09-22 21:40:14 -0300

    [dev] Gabriel - Fonseca

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,12 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = async (product) => {
+  try {
+    const url = `https://api.mercadolibre.com/sites/MLB/search?q=${product}`;
+    const response = await fetch(url);
+    const object = await response.json();
+    return object.result;
+  } catch (error) {
+    throw new Error('You must provide an url');
+  }
 };
 
 if (typeof module !== 'undefined') {
